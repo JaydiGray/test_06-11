7b3bb755694d932e998935b2867567952ec26580
JaydiGray <evshamatrin@gmail.com>
2024-11-07 05:53:58 +0300

    second

---
+++ src/js/api-service.js
@@ -0,0 +1,37 @@
+export default class ApiService {
+  constructor() {
+    this.searchQuery = '';
+    this.page = 1;
+  }
+
+  searchPerRequest() {
+    const API_KEY = '46928104-60edbd0ee49e3ce36068a60a9';
+    const BASE_URL = 'https://pixabay.com/api/';
+
+    return fetch(
+      `${BASE_URL}?key=${API_KEY}&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&page=${this.page}&per_page=40`
+    )
+      .then(res => res.json())
+      .then(data => {
+        this.incrementPage();
+        console.log(data);
+        return data;
+      });
+  }
+
+  incrementPage() {
+    this.page += 1;
+  }
+
+  resetPage() {
+    this.page = 1;
+  }
+
+  get query() {
+    return this.searchQuery;
+  }
+
+  set query(newQuery) {
+    this.searchQuery = newQuery;
+  }
+}
--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,132 @@
-console.log('hello');
+import Notiflix from 'notiflix';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import ApiService from './api-service';
+import LoadMoreBtn from './load-more-btn';
+import LoadMoreBtn from './load-more-btn';
+
+const refs = {
+  searchForm: document.querySelector('#search-form'),
+  gallery: document.querySelector('.gallery'),
+};
+let gallery;
+const loadMoreBtn = new LoadMoreBtn({
+  selector: '[data-action="load-more"]',
+  hidden: true,
+});
+const apsService = new ApiService();
+
+refs.searchForm.addEventListener('submit', onSearch);
+loadMoreBtn.refs.button.addEventListener('click', onLoadMore);
+
+function onSearch(e) {
+  e.preventDefault();
+
+  apsService.query = e.currentTarget.elements.searchQuery.value;
+  apsService.resetPage();
+  clearGalleryContainer();
+
+  apsService
+    .searchPerRequest()
+    .then(data => {
+      if (!data.hits.length) {
+        Notiflix.Notify.failure(
+          'Sorry, there are no images matching your search query. Please try again.'
+        );
+        return;
+      }
+
+      loadMoreBtn.show();
+      refs.searchForm.elements.searchQuery.value = '';
+      console.log(refs.searchForm.elements.searchQuery.value);
+      Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images!`);
+      createTemplate(data.hits);
+
+      gallery = new SimpleLightbox('.gallery a');
+      gallery.on('show.simplelightbox', function () {});
+    })
+    .catch(error => {
+      Notiflix.Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+      throw new Error(error);
+    });
+}
+
+function onLoadMore() {
+  loadMoreBtn.disable();
+
+  apsService
+    .searchPerRequest()
+    .then(data => {
+      if ((apsService.page - 2) * 40 >= data.total) {
+        loadMoreBtn.hide();
+        Notiflix.Notify.info(
+          "We're sorry, but you've reached the end of search results."
+        );
+        return;
+      }
+
+      if (!data.hits.length) {
+        Notiflix.Notify.failure(
+          'Sorry, there are no images matching your search query. Please try again.'
+        );
+        return;
+      }
+
+      loadMoreBtn.enable();
+      Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images!`);
+      createTemplate(data.hits);
+
+      gallery.refresh();
+    })
+    .catch(error => {
+      Notiflix.Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+      throw new Error(error);
+    });
+}
+
+function createTemplate(articles) {
+  refs.gallery.insertAdjacentHTML(
+    'beforeend',
+    articles
+      .map(
+        ({
+          webformatURL,
+          largeImageURL,
+          tags,
+          likes,
+          views,
+          comments,
+          downloads,
+        }) => `<a href="${largeImageURL}" class="link"><div class='photo-card'>
+    <img src='${webformatURL}' alt='${tags}' loading='lazy' height='260' />
+    <div class='info'>
+      <p class='info-item'>
+        <b>Likes:</b>
+        <span>${likes}</span>
+      </p>
+      <p class='info-item'>
+        <b>Views:</b>
+        <span>${views}</span>
+      </p>
+      <p class='info-item'>
+        <b>Comments:</b>
+        <span>${comments}</span>
+      </p>
+      <p class='info-item'>
+        <b>Downloads:</b>
+        <span>${downloads}</span>
+      </p>
+    </div>
+  </div></a>`
+      )
+      .join('')
+  );
+}
+
+function clearGalleryContainer() {
+  refs.gallery.innerHTML = '';
+}
